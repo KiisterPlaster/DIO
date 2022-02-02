112a01ed020c7eb78f5d018bcfa5310703291531
KiisterPlaster <maikon.jipa@gmail.com>
2022-02-02 01:57:28 -0400

    Curso de Java (Variaveis)

---
+++ JAVA/src/com/variaveis/Variaveis.java
@@ -0,0 +1,60 @@
+//Imports
+
+package com.variaveis;
+
+/**
+ * @since Release 0.0.1
+ * @version 0.0.1
+ * @author By KiisterPlaster
+ */
+
+
+public class Variaveis {
+
+    public static void main(String[] args) {
+
+        int i;
+        //int i;
+        int I;
+        //int 1a;
+        int _1a;
+        int $1a;
+
+        i = 1;
+        I = 10;
+        _1a = 20;
+        $1a = 7;
+
+        
+        final int j = 10;
+        //j = 15;
+        int asrn2456md;
+        //int asrn2456 md;
+        int asrn$2456_md;
+        //int asrn%2456_md;
+        
+        asrn2456md = 100;
+        asrn$2456_md = 10;
+
+        int quantidadeProduto = 50;
+        //int QuantidadeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        //final int numeroTentativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+        //int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($1a);
+        
+        System.out.println(j);
+        System.out.println(asrn2456md);
+        System.out.println(asrn$2456_md);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+    }
+
+}
