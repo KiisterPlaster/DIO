f159830d36ec00394a1663039298a7296d6f3a8d
KiisterPlaster <maikon.jipa@gmail.com>
2022-02-03 00:01:32 -0400

    Curso de Java (Dados)

---
+++ Java/src/com/dados/Dados.java
@@ -0,0 +1,64 @@
+// Imports
+
+package com.dados;
+
+/**
+ * @since Release 0.0.1
+ * @version 0.0.1
+ * @author By KiisterPlaster
+ */
+
+public class Dados {
+    
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short sh1 = 10000;
+        short sh2 = 20000;
+
+        int i1 = 100000000;
+        int i2 = 200000000;
+
+        long l1 = 1000000000000000L;
+        long l2 = 2000000000000000L;
+
+        float f1 = 1.00F;
+        float f2 = 2.00F;
+
+        double d1 = 1.00;
+        double d2 = 2.00;
+
+        char c1 = 'W';
+        char c2 = '\u0054';
+
+        String s1 = "null";
+        String s2 = "meu amigo";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(sh1);
+        System.out.println(sh2);
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+    }
+    
+}
